15a7e1807841f1888461f8fb1c7c26491d318718
Dawsonkw <dawsonkw@gmail.com>
2024-08-20 17:37:04 -0600

    okay, i think that worked. this has been a lesson in hardship. flask was so much more lightweight

---
+++ Backend/recipeproject/recipes/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.URLField()
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    servings = models.IntegerField()
+    prep_time = models.CharField(max_length=50)
+    cook_time = models.CharField(max_length=50)
+    category = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
