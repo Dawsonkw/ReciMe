a15ef657d0de840a1d9810b9be0e0ec37a294f71
Dawsonkw <dawsonkw@gmail.com>
2024-08-21 19:37:59 -0600

    I got all the data piped in to the app, need to now work some styling. Images are all placeholder so im gonna try to just use a stockphoto for them all. gonna set up a POST request to add recipes then its all QOL stuff

--- Frontend/src/components/recipes.tsx
+++ Frontend/src/components/recipes.tsx
@@ -1,7 +1,27 @@
+import { useFetchRecipes } from "../lib/hooks";
+
 function Recipes() {
+  const { recipes, loading, error } = useFetchRecipes();
   return (
-    <div>
-      <p>buncha little guy recipes</p>
+    <div className="flex flex-wrap justify-center">
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="flex flex-col p-4 max-w-[25%]  ">
+          <div className="flex-col ">
+            <img src={recipe.image} alt={recipe.title} className="mx-auto" />
+            <h2 className="justify-center items-center text-4xl text-center ">
+              {recipe.title}
+            </h2>
+            <p className="text-wrap">{recipe.description}</p>
+            <p>{recipe.ingredients}</p>
+            <p>{recipe.instructions}</p>
+            <p>{recipe.servings}</p>
+            <p>{recipe.prepTime}</p>
+            <p>{recipe.cookTime}</p>
+            <p>{recipe.category}</p>
+            <p>{recipe.difficulty}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ Frontend/src/lib/hooks.tsx
@@ -0,0 +1,49 @@
+// Endpoint: http://127.0.0.1:8000/api/recipes/
+// GET request to get all recipes
+// POST request to create a new recipe
+
+import { useEffect, useState } from "react";
+import { BASE_API_URL } from "./constants";
+
+type RecipeApiResponse = {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  ingredients: string;
+  instructions: string;
+  servings: number;
+  prepTime: number;
+  cookTime: number;
+  category: string;
+  difficulty: string;
+};
+
+export const useFetchRecipes = () => {
+  const [recipes, setRecipes] = useState<RecipeApiResponse[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(BASE_API_URL);
+        if (!response.ok) {
+          const errorData = await response.text;
+          throw new Error(`API request failed ${errorData}`);
+        }
+        const data: RecipeApiResponse[] = await response.json();
+        setRecipes(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching recipes", error);
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchRecipes();
+  }, []);
+
+  return { recipes, loading, error };
+};
