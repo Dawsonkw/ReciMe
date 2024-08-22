381f7e9eba3f9469e513e22de0789abcabcd15bf
Dawsonkw <dawsonkw@gmail.com>
2024-08-21 20:00:10 -0600

    django is confusing. updated to a stock photo for placeholder

--- Backend/recipeproject/recipes/models.py
+++ Backend/recipeproject/recipes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     image = models.URLField()
     description = models.TextField()
     ingredients = models.TextField()
